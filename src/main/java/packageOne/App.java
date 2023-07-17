34575c29253aee45b3293d739c948f3586f0b1fe
unknown <tejashree@gmail.com>
2023-07-17 11:40:43 -0400

    updated to default

---
+++ src/main/java/packageOne/App.java
@@ -0,0 +1,13 @@
+package packageOne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
